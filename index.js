ff6b19ea4ad7251bb27c27f3c5014a7f63fc2a36
MarcosWinther <winthermarcos@gmail.com>
2025-06-11 21:55:03 -0300

    :recycle: refactor: Melhorando a lógica do código do projeto

--- index.js
+++ index.js
@@ -22,21 +22,24 @@
 
 nomeHeroi = "Zelda"
 xp = 8000
+nivel = ""
 
 if (xp <= 1000) {
-   console.log("Ferro")
+   nivel = "Ferro"
 } else if ((xp > 1000) && (xp <= 2000)) {
-   console.log("Bronze")
+   nivel = "Bronze"
 } else if ((xp > 2000) && (xp <= 5000)) {
-   console.log("Prata Ouro")
+   nivel = "Prata"
 } else if ((xp > 5000) && (xp <= 7000)) {
-   console.log("Ouro")
+   nivel = "Ouro"
 } else if ((xp > 7000) && (xp <= 8000)) {
-   console.log("Platina")
+   nivel = "Platina"
 } else if ((xp > 8000) && (xp <= 9000)) {
-   console.log("Ascendente")
+   nivel = "Ascendente"
 } else if ((xp > 9000) && (xp <= 10000)) {
-   console.log("Imortal")
+   nivel = "Imortal"
 } else if (xp > 10000) {
-   console.log("Radiante")
+   nivel = "Radiante"
 }
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
